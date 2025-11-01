de895196dcb9a16469a2de95eff85bccd3b429c1
Maximiliano Correa <jmaxicorrea@gmail.com>
2025-11-01 00:24:56 -0300

    Initial Project Archetype

---
+++ src/main/java/com/github/maxicorrea/java_spring_boot_rate_limit/JavaSpringBootRateLimitApplication.java
@@ -0,0 +1,13 @@
+package com.github.maxicorrea.java_spring_boot_rate_limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootRateLimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootRateLimitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maxicorrea/java_spring_boot_rate_limit/JavaSpringBootRateLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maxicorrea.java_spring_boot_rate_limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootRateLimitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
